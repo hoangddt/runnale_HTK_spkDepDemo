3d911d2d5944c0899866f21c1385047028f2c348
hoangddt <quochoangddt@gmail.com>
2017-11-20 14:11:00 +0700

    Add result of run state: 9 mixture 1

--- run_copy_result.sh
+++ run_copy_result.sh
@@ -3,5 +3,6 @@
 ./runDemo
 mkdir Experiments/state$1_mix$2
 cp -r result Experiments/state$1_mix$2
-git add -A && git commit -m "Add result of run state: $1 mixture $2" &
+git add -A
+git commit -m "Add result of run state: $1 mixture $2" &>/dev/null
 echo "Done"
