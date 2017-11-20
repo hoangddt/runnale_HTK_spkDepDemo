3159793be2066c491b22d57aa72a662c5179b4a4
hoangddt <quochoangddt@gmail.com>
2017-11-20 14:20:45 +0700

    Update report conslusion to file

--- run_copy_result.sh
+++ run_copy_result.sh
@@ -5,4 +5,9 @@ mkdir Experiments/state$1_mix$2
 cp -r result Experiments/state$1_mix$2
 git add -A
 git commit -m "Add result of run state: $1 mixture $2" &>/dev/null
+
+
+echo "reporting"
+tail Experiments/state$1_mix$2/result/testResult Experiments/state$1_mix$2/result/trainResult -n 4
+tail Experiments/state$1_mix$2/result/testResult Experiments/state$1_mix$2/result/trainResult -n 4 > Experiments/reports/state$1_mix$2
 echo "Done"
